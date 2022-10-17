9a42d2742b2e064ae61daaf616ce02e0c263f4de
Tanvi Vernekar <tanvivernekar@TanviV-iMac.local>
2022-10-17 20:04:17 +0530

    added splash

--- src/navigations/Router.js
+++ src/navigations/Router.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RNBootSplash from 'react-native-bootsplash'
 import MainScreen from '../screens/MainScreen';
 import AppScreen from '../screens/AppScreen';
 
@@ -13,7 +14,7 @@ const theme = {
   };
 const Router=()=>{
     return(
-        <NavigationContainer theme={theme} >
+        <NavigationContainer theme={theme} onReady={()=> RNBootSplash.hide()} >
         <Stack.Navigator initialRouteName="MainScreen">
           <Stack.Screen name="MainScreen" component={MainScreen} options={{headerShown:false}}/>
           <Stack.Screen name="AppScreen" component={AppScreen} />
